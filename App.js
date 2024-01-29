63e542ca35af3923e323c3febadd0df363aa114b
NgoDinhTin <tinndpd09005@fpt.edu.vn>
2024-01-29 11:17:03 +0700

    ff

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Splash from './scr/screen/auth/Splash';
 import Lab5 from './scr/screen/lab5';
+import Lab6 from './scr/screen/lab6';
 
 const App = () => {
   return (
-   <Lab5></Lab5>
+   <Lab6></Lab6>
   );
 };
 
